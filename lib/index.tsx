dc965a1ef84e30ec67afea28c3307845bc8eb4ca
niuzhiwei <xujin02@qq.com>
2020-11-26 21:50:02 +0800

    添加webpack的externals

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Button from './button'
+
+ReactDOM.render(<Button></Button>,document.body)
 
 console.log(React)
-console.log(ReactDOM)
