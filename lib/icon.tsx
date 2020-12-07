8b27c168f4987fc00482445aca5747491053fe9a
niuzhiwei <xujin02@qq.com>
2020-12-07 20:50:21 +0800

    添加onclick

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './importicons.js';
 import './icon.scss';
+
 interface IconProps{
     name:string;
+    onClick:React.MouseEventHandler<SVGElement>;
 }
 
 const Icon:React.FunctionComponent<IconProps> =(props)=>{
     return(
-        <svg  className="fui-icon">
+        <svg  className="fui-icon" onClick={props.onClick}>
             <use xlinkHref={`#${props.name}`}></use>
         </svg>
     );
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Icon from './icon';
-
-ReactDOM.render(<div><Icon name="wechat" /><Icon name="qq" /><Icon name="alipay" /></div>,document.querySelector('#root'));
+const fn:React.MouseEventHandler<Element> = (e)=>{
+    console.log(e.target)
+};
+ReactDOM.render(<div><Icon name="wechat" onClick={fn} /></div>,document.querySelector('#root'));
