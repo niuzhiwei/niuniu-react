0d6f809145112f0936c8e17b9259bf188dfbe72f
niuzhiwei <xujin02@qq.com>
2020-12-07 20:26:46 +0800

    添加css

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './importicons.js';
-
+import './icon.scss';
 interface IconProps{
     name:string;
 }
 
 const Icon:React.FunctionComponent<IconProps> =(props)=>{
-    return <span>
-        <svg>
+    return(
+        <svg  className="fui-icon">
             <use xlinkHref={`#${props.name}`}></use>
         </svg>
-    </span>;
+    );
 };
 export default Icon;
