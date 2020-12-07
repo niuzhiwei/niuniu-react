5e60e248b25441893f9dbc0012f30d52c22a9b99
niuzhiwei <xujin02@qq.com>
2020-12-07 21:29:47 +0800

    简化参数

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -8,11 +8,10 @@ interface IconProps extends React.SVGAttributes<SVGElement>
     name:string;
 }
 
-const Icon:React.FunctionComponent<IconProps> =(props)=>{
-    const {className,...restProps} = props
+const Icon:React.FunctionComponent<IconProps> =({className,name,...restProps})=>{
     return(
         <svg  className={classes('fui-icon',className)} {...restProps}>
-            <use xlinkHref={`#${props.name}`}></use>
+            <use xlinkHref={`#${name}`}></use>
         </svg>
     );
 };
