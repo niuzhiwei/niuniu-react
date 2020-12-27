c265db7525c76dcb51c2bac4c8fb8f5a23607ba8
niuzhiwei <xujin02@qq.com>
2020-12-27 13:23:18 +0800

    npm publish 配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Icon from './icon/icon';
-const fn:React.MouseEventHandler<Element> = (e)=>{
-    console.log(e.target)
-};
-const fn1:React.MouseEventHandler<Element>=(e)=>{
-    console.log(e)
-}
-ReactDOM.render(<div><Icon className='wechat' name="wechat" onClick={fn} onMouseEnter={fn1} /></div>,document.querySelector('#root'));
+// import ReactDOM from 'react-dom';
+// import React from 'react';
+export {default as Icon} from './icon/icon'
+// const fn:React.MouseEventHandler<Element> = (e)=>{
+//     console.log(e.target)
+// };
+// const fn1:React.MouseEventHandler<Element>=(e)=>{
+//     console.log(e)
+// }
+// ReactDOM.render(<div><Icon className='wechat' name="wechat" onClick={fn} onMouseEnter={fn1} /></div>,document.querySelector('#root'));
