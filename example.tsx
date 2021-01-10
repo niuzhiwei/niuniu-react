51501038d97b7f2df1127a8d174531530e7fa9d4
niuzhiwei <xujin02@qq.com>
2021-01-10 17:36:50 +0800

    完成example页面布局

--- example.tsx
+++ example.tsx
@@ -4,31 +4,37 @@ import {HashRouter as Router,Link,Route} from 'react-router-dom'
 import  IconExample  from './lib/icon/icon.example';
 import DialogExample from './lib/dialog/dialog.example';
 import LayoutExample from './lib/layout/layout.example';
+import {Layout,Header,Aside,Content,Footer} from './lib/layout/layout'
+import './example.scss'
+const logo = require('./logo.png');
 
 ReactDOM.render(
 <Router>
-    <div>
-        <header>
+    <Layout className='site-page'>
+        <Header className="site-header">
             <div className="logo">
-                NiuNiu UI
+                <img src={logo.default} width='48' height='48' alt=""/>
+                <span>NiuNiu UI</span>
             </div>
-        </header>
-        <div>
-            <aside>
+        </Header>
+        <Layout>
+            <Aside className='site-aside'>
                 <h2>组件</h2>
                 <ul>
                     <li><Link to='/icon'>Icon</Link></li>
                     <li><Link to='/dialog'>Dialog</Link></li>
                     <li><Link to='/layout'>Layout</Link></li>
                 </ul>
-            </aside>
-            <main>
+            </Aside>
+            <Content className='site-main'>
              <Route path='/icon' component={IconExample} ></Route>
              <Route path='/dialog' component={DialogExample} ></Route>
              <Route path='/layout' component={LayoutExample} ></Route>
-            </main>
-        </div>
-       
-    </div>
+            </Content>
+        </Layout>
+       <Footer className='site-footer'>
+           &copy; niuniu
+       </Footer>
+    </Layout>
     </Router>
 ,document.querySelector('#root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const path = require('path')
 module.exports = {
   entry: {
-    FUI: './lib/index.tsx'
+    FUI: './lib/index.tsx',
   },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
     library: 'FUI',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'jsx']
+    extensions: ['.ts', '.tsx', '.js', 'jsx'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'awesome-typescript-loader',
       },
       {
         test: /\.svg$/,
-        loader: 'svg-sprite-loader'
+        loader: 'svg-sprite-loader',
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
 }
