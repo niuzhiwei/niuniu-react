d8c12cf3cb5dc71bffb6f12056ab12aa94feb454
niuzhiwei <xujin02@qq.com>
2021-02-19 09:19:50 +0800

    增加input组件

---
+++ lib/input/input.tsx
@@ -0,0 +1,12 @@
+import React, { InputHTMLAttributes } from 'react'
+import classes from '../helpers//classnames'
+import './input.scss'
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> { }
+const Input: React.FunctionComponent<Props> = (props) => {
+    const { className, ...rest } = props
+    return (
+        <input className={classes('fui-input', className)} {...rest} />
+    )
+}
+export default Input
