9392a89e963f1180761a10fd70145777ead744c9
niuzhiwei <xujin02@qq.com>
2021-02-26 20:41:06 +0800

    优化useUpdate

--- lib/hooks/useUpdate.tsx
+++ lib/hooks/useUpdate.tsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 const useUpdate = (dep: boolean, fn: () => void) => {
-    const [count, setCount] = useState(0);
+    const isFirst = useRef(true)
     useEffect(() => {
-        setCount(x => x + 1)
-    }, [dep])
-    useEffect(() => {
-        if (count > 1) {
-            fn()
+        if (isFirst.current === true) {
+            isFirst.current = false;
+            return
         }
-    }, [count])
+        fn();
+    }, [dep])
 }
 export default useUpdate
