0fddb0b5c1ef672dc6cda1a853ccc7a14147886f
niuzhiwei <xujin02@qq.com>
2021-02-27 10:55:35 +0800

    tree组件添加动画

--- lib/tree/tree-item.tsx
+++ lib/tree/tree-item.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, useState } from 'react';
+import React, { ChangeEventHandler, useRef, useState } from 'react';
 import { scopedClassMaker } from '../helpers/classnames';
 import { SourceDataItem, TreeProps } from './SourceDataItem';
 import useUpdate from '../hooks/useUpdate';
@@ -39,7 +39,36 @@ const TreeItem: React.FunctionComponent<treeProps> = (props) => {
         setExpanded(false)
     }
     const [expanded, setExpanded] = useState(true);
-    useUpdate(expanded, () => { console.log(expanded) });
+    const divRef = useRef<HTMLDivElement>(null);
+    useUpdate(expanded, () => {
+        if (!divRef.current) { return }
+        if (expanded) {
+            divRef.current.style.height = 'auto';
+            const { height } = divRef.current.getBoundingClientRect();
+            divRef.current.style.height = '0px'
+            divRef.current.getBoundingClientRect();
+            divRef.current.style.height = height + 'px';
+            const afterExpand = () => {
+                if (!divRef.current) { return }
+                divRef.current.style.height = '';
+                divRef.current.classList.add('fui-tree-children-present');
+                divRef.current.removeEventListener('transitionend', afterExpand)
+            }
+            divRef.current.addEventListener('transitionend', afterExpand)
+        } else {
+            const { height } = divRef.current.getBoundingClientRect();
+            divRef.current.style.height = height + 'px';
+            divRef.current.getBoundingClientRect();
+            divRef.current.style.height = '0px';
+            const afterCollapse = () => {
+                if (!divRef.current) { return }
+                divRef.current.style.height = '';
+                divRef.current.classList.add('fui-tree-children-gone');
+                divRef.current.removeEventListener('transitionend', afterCollapse)
+            }
+            divRef.current.addEventListener('transitionend', afterCollapse)
+        }
+    });
     return <div
         key={item.value}
         className={sc(classes)}>
@@ -54,7 +83,7 @@ const TreeItem: React.FunctionComponent<treeProps> = (props) => {
                     {expanded ? <span onClick={collapse}>-</span> : <span onClick={expand}>+</span>}
                 </span>}
         </div>
-        <div className={sc({ children: true, collapsed: !expanded })}>
+        <div ref={divRef} className={sc({ children: true, collapsed: !expanded })}>
             {item.children?.map(sub => {
                 return <TreeItem key={sub.value} item={sub} level={level + 1} treeProps={treeProps}></TreeItem>
             })}
